18d1f3fa664a0679eeaa0620e5c11c0782bb49f4
Andree Huk <andree@blended.io>
2017-06-14 17:22:42 +0200

    added for loop to download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 # Pull the latest changes from the repo
 git pull
 
-files="web-ui-kit-marcosilva" # add a new file like this: "file1 file2 file3"
+files="web-ui-kit-marcosilva web-ui-kit-marcosilva2" # add a new file like this: "file1 file2 file3"
 for f in $files
 do
   # Zip core Sketch data
@@ -13,5 +13,5 @@ do
   cp $f.zip ../$f.sketch
 
   # Remove zip
-  rm -Rf $f.zip
+  rm -Rf $f.zip && cd ..
 done
