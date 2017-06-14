df30a4fa29d2c9aafea717e2c5e60b41459a8b7e
Andree Huk <andree@blended.io>
2017-06-14 17:18:43 +0200

    added for loop

--- download.sh
+++ download.sh
@@ -3,11 +3,15 @@
 # Pull the latest changes from the repo
 git pull
 
-# Zip core Sketch data
-cd sketch-data/ && zip web-ui-kit-marcosilva.zip -r .
+files="web-ui-kit-marcosilva" # add a new file like this: "file1 file2 file3"
+for f in $files
+do
+  # Zip core Sketch data
+  cd $f/ && zip $f.zip -r .
 
-# Copy .zip to .sketch
-cp web-ui-kit-marcosilva.zip ../web-ui-kit-marcosilva.sketch
+  # Copy .zip to .sketch
+  cp $f.zip ../$f.sketch
 
-# Remove zip
-rm -Rf web-ui-kit-marcosilva.zip
+  # Remove zip
+  rm -Rf $f.zip
+done
--- upload.sh
+++ upload.sh
@@ -3,17 +3,22 @@
 # Pull the latest changes from the repo
 git pull
 
-# Copy .sketch to .zip
-cp sketch-test.sketch sketch-test.zip
+files="web-ui-kit-marcosilva" # add a new file like this: "file1 file2 file3"
+for f in $files
+do
+  # Copy .sketch to .zip
+  cp $f.sketch $f.zip
 
-# Unzip the file and delete
-unzip -o sketch-test.zip -d sketch-data/ && rm -Rf sketch-test.zip
+  # Unzip the file and delete
+  unzip -o $f.zip -d $f/
+  rm -Rf $f.zip
 
-# Remove the preview file
-rm -Rf sketch-data/previews/
+  # Remove the preview file
+  rm -Rf $f/previews/
 
-git add sketch-data/
-git add sketch-data/pages/
+  git add $f/
+done
+
 git add upload.sh
 git add download.sh
 
