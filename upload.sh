9aaca5d08d4ccbc995fe4a3e21bcd9e6e8106e84
Andree Huk <andree@blended.io>
2017-06-14 17:20:32 +0200

    testing for loop

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 # Pull the latest changes from the repo
 git pull
 
-files="web-ui-kit-marcosilva" # add a new file like this: "file1 file2 file3"
+files="web-ui-kit-marcosilva web-ui-kit-marcosilva2" # add a new file like this: "file1 file2 file3"
 for f in $files
 do
   # Copy .sketch to .zip
